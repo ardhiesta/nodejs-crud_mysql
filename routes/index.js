34f7d93fe10ca7c7ad90f636b4345a15112bb42f
ardhiesta <ardhi.mail@gmail.com>
2018-03-21 20:52:41 +0700

    redirect / ke /students

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var mysql = require('mysql');
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "linuxluv",
-  password: "linuxluv",
-  database: "db_student"
-});
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-  });
-  
-  res.send('express');    
+/* GET home page
+redirect ke /students */
+router.get('/', function(req, res, next) { 
+  res.redirect('/students');
 });
 
 module.exports = router;
